61aa8881d94eced1409052c088c53e1ad3631c93
cs343 <cs343@4737daf4-6215-0410-ac27-f260304238ed>
2004-07-01 22:15:57 +0000

    InferenceRule now implements the interface IterationElement

--- trunk/bddbddb/org/sf/bddbddb/InferenceRule.java
+++ trunk/bddbddb/org/sf/bddbddb/InferenceRule.java
@@ -30,7 +30,7 @@ import org.sf.bddbddb.util.Navigator;
  * @author jwhaley
  * @version $Id$
  */
-public abstract class InferenceRule {
+public abstract class InferenceRule implements IterationElement{
     
     List/*<RuleTerm>*/ top;
     RuleTerm bottom;
