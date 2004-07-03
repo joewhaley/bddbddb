6cc1ab8f323714b897cf86eebb5c2fc1569ab977
joewhaley <joewhaley@4737daf4-6215-0410-ac27-f260304238ed>
2004-07-03 07:50:44 +0000

    Eliminate evil tabs.

--- trunk/bddbddb/org/sf/bddbddb/IterationElement.java
+++ trunk/bddbddb/org/sf/bddbddb/IterationElement.java
@@ -1,17 +1,17 @@
 /*
  * Created on Jun 29, 2004
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * 
+ * TODO To change the template for this generated file go to Window -
+ * Preferences - Java - Code Style - Code Templates
  */
 package org.sf.bddbddb;
 
 /**
  * @author mcarbin
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * 
+ * TODO To change the template for this generated type comment go to Window -
+ * Preferences - Java - Code Style - Code Templates
  */
 public interface IterationElement {
-	public boolean update();
+    public boolean update();
 }
