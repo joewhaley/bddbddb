5f6f4805bdb71e457a140b541e581c06ea7430bb
cs343 <joewhaley@users.noreply.github.com>
2004-07-08 01:57:28 +0000

    Updated applyex with new function names (getrelationdest)

--- bddbddb/org/sf/bddbddb/ir/lowlevel/ApplyEx.java
+++ bddbddb/org/sf/bddbddb/ir/lowlevel/ApplyEx.java
@@ -23,15 +23,18 @@ import org.sf.javabdd.BDDFactory.BDDOp;
 public class ApplyEx extends LowLevelOperation {
 
     BDDRelation r0, r1, r2;
+
     BDDOp op;
-    List/*<Attribute>*/ attributes;
+
+    List/* <Attribute> */attributes;
 
     /**
      * @param r0
      * @param r1
      * @param r2
      */
-    public ApplyEx(BDDRelation r0, BDDRelation r1, BDDOp op, BDDRelation r2, List/*<Attribute>*/ attributes) {
+    public ApplyEx(BDDRelation r0, BDDRelation r1, BDDOp op, BDDRelation r2,
+        List/* <Attribute> */attributes) {
         this.r0 = r0;
         this.r1 = r1;
         this.r2 = r2;
@@ -39,31 +42,50 @@ public class ApplyEx extends LowLevelOperation {
         this.attributes = attributes;
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.sf.bddbddb.ir.lowlevel.LowLevelOperation#visit(org.sf.bddbddb.ir.lowlevel.LowLevelOperationVisitor)
      */
     public Object visit(LowLevelOperationVisitor i) {
         return i.visit(this);
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.sf.bddbddb.ir.Operation#toString()
      */
     public String toString() {
-        String opName;
-        if (op == BDDFactory.and) opName = "relprod";
-        else opName = op.toString()+"Ex";
-        return r0.toString()+" = "+opName+"("+r1.toString()+","+r2.toString()+","+attributes+")";
+
+        return r0.toString() + " = " + getExpressionString();
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.sf.bddbddb.ir.Operation#getExpressionString()
+     */
+    public String getExpressionString() {
+        String opName;
+        if (op == BDDFactory.and) opName = "relprod";
+        else opName = op.toString() + "Ex";
+        return opName + "(" + r1.toString() + "," + r2.toString() + ","
+            + attributes + ")";
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.sf.bddbddb.ir.Operation#getDest()
      */
-    public Relation getDest() {
+    public Relation getRelationDest() {
         return r0;
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.sf.bddbddb.ir.Operation#getSrcs()
      */
     public List getSrcs() {
@@ -76,7 +98,7 @@ public class ApplyEx extends LowLevelOperation {
     public Relation getSrc1() {
         return r1;
     }
-    
+
     /**
      * @return Returns the source relation.
      */
@@ -97,14 +119,13 @@ public class ApplyEx extends LowLevelOperation {
         return b;
     }
 
-    
     /**
      * @return Returns the attributes.
      */
     public List getAttributes() {
         return attributes;
     }
-    
+
     /**
      * @return
      */
