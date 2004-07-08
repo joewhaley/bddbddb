235e13ee724ea3e158e05d7ea277fcbc17e6128b
cs343 <cs343@4737daf4-6215-0410-ac27-f260304238ed>
2004-07-08 01:55:24 +0000

    Adding dynamic ir stuff

---
+++ trunk/bddbddb/org/sf/bddbddb/ir/dynamic/DynamicOperation.java
@@ -0,0 +1,25 @@
+package org.sf.bddbddb.ir.dynamic;
+
+import org.sf.bddbddb.ir.Operation;
+import org.sf.bddbddb.ir.OperationVisitor;
+
+/**
+ * @author Collective
+ */
+public abstract class DynamicOperation extends Operation {
+
+    /**
+     * @param i
+     * @return
+     */
+    public Object visit(OperationVisitor i) {
+        return visit((DynamicOperationVisitor) i);
+    }
+
+    /**
+     * @param i
+     * @return
+     */
+    public abstract Object visit(DynamicOperationVisitor i);
+
+}
