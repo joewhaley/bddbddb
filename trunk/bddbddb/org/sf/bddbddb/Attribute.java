6c924a5dd3d6b3d1d80881169526cb8afd82bd42
cs343 <cs343@4737daf4-6215-0410-ac27-f260304238ed>
2004-07-12 20:28:54 +0000

    atrribute update

--- trunk/bddbddb/org/sf/bddbddb/Attribute.java
+++ trunk/bddbddb/org/sf/bddbddb/Attribute.java
@@ -33,14 +33,23 @@ public class Attribute {
     public String toString() {
         return attributeName;
     }
-    
+
     /**
      * @return
      */
     public Domain getDomain() {
         return attributeDomain;
     }
-    
+
+    public boolean equals(Object o) {
+        if (o instanceof Attribute) {
+            Attribute that = (Attribute) o;
+            return attributeName.equals(that.attributeName) && attributeDomain.name.equals(that.attributeDomain.name)
+                && attributeOptions.equals(that.attributeOptions);
+        }
+        return false;
+    }
+
     /**
      * @return
      */
