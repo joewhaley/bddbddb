2f987d2498e492fe647a8a95aea4a36102dc730a
joewhaley <joewhaley@users.noreply.github.com>
2004-10-16 02:45:30 +0000

    Renamed org.sf.bddbddb to net.sf.bddbddb

---
+++ bddbddb/net/sf/bddbddb/ir/dynamic/DynamicInterpreter.java
@@ -0,0 +1,13 @@
+// DynamicInterpreter.java, created Jun 29, 2004 12:50:19 PM 2004 by jwhaley
+// Copyright (C) 2004 John Whaley <jwhaley@alum.mit.edu>
+// Licensed under the terms of the GNU LGPL; see COPYING for details.
+package net.sf.bddbddb.ir.dynamic;
+
+/**
+ * DynamicInterpreter
+ * 
+ * @author jwhaley
+ * @version $Id$
+ */
+public interface DynamicInterpreter extends DynamicOperationVisitor {
+}
